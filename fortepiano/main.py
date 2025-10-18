738e753b41184ecb97de2b467e8fc1e7c134f386
egorQ2011 <egor.mogila2011@gmail.com>
2025-10-18 11:29:06 +0300

    Add files via upload

---
+++ fortepiano/main.py
@@ -0,0 +1,13 @@
+from pygame import *
+from settings import WINDOW_WIDTH, WINDOW_HEIGHT, WHITE
+
+init()
+screen = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+running = True
+while running:
+               for e in event.get():
+                       if e.type == QUIT:
+                               running = False
+               screen.fill(WNITE)
+               display.update()
+                              
